a180fad215558c11f2128f82e6ff87ddf2f0204b
Becky Tate <mrtate@us.ibm.com>
2016-02-26 14:30:30 -0500

    Adding classes corresponding with chapter 1 and chapter 2 of scala book

--- src/ScalaApp.scala
+++ src/ScalaApp.scala
@@ -3,7 +3,14 @@
  */
 object ScalaApp {
 
-  def main(args: Array[String]): Unit ={
-    print("Hello World")
+  def main(args: Array[String]): Unit = {
+    val cafe = new Cafe()
+    val coffees = cafe.buyCoffees(new CreditCard(), 3)
+    print(coffees)
+  }
+
+
+  def coalesce(charges: List[Charge]) : List[Charge] = {
+    charges.groupBy(_.cc).values.map(_.reduce(_ combine _)).toList
   }
 }
